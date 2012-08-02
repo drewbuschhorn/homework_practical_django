0fd07a4c2bd67daf265a6f88dcc38a0b18e5d7f1
Drew Buschhorn <drewbuschhorn@gmail.com>
2012-08-01 22:44:51 -0400

    Switched to Dj1.0 way of doing things.

--- cms/search/models.py
+++ cms/search/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.contrib.flatpages.models import FlatPage
 
-# Create your models here.
+## See https://docs.djangoproject.com/en/dev/releases/1.0-porting-guide/
+class SearchKeyword(models.Model):
+	keyword = models.CharField(max_length=50) # ,core=True) ## core has been deprecated in dj1.0+
+	page	= models.ForeignKey(FlatPage) #, edit_inline=models.STACKED, 
+					# min_num_in_admin=3, num_extra_on_change=1) ## deprecated in dj1.0+
+
+	#Converted to DJ1.0 way of doing things, see admin.py
+	#class Admin:
+	#	pass
+
+	def __unicode__(self):
+		return self.keyword
+
+
--- cms/search/views.py
+++ cms/search/views.py
@@ -2,9 +2,14 @@ from django.shortcuts import render_to_response
 from django.contrib.flatpages.models import FlatPage
 
 def search(request):
-	query = request.GET['q']
+	query = request.GET.get('q','')
+	results = []
+	if query:
+		keyword_results = FlatPage.objects.filter(searchkeyword__keyword__in=query.split()).distinct()
+		results = FlatPage.objects.filter(content__icontains=query)
 	return render_to_response('search/search.html',
 			{ 	'query': query,
-				'results': FlatPage.objects.filter(content__icontains=query),
+				'keyword_results': keyword_results,
+				'results': results,
 			})
 
