87b9dbd5a2edcd2600615f25df65382dcbce72b6
Drew Buschhorn <drewbuschhorn@gmail.com>
2012-08-05 17:32:07 -0400

    Adding Entry model to Coltrane.

--- cms/coltrane/admin.py
+++ cms/coltrane/admin.py
@@ -1,7 +1,14 @@
 from django.contrib import admin
-from coltrane.models import Category
+from coltrane.models import Category, Entry
 
 class CategoryAdmin(admin.ModelAdmin):
 	model = Category
+	prepopulated_fields = {'slug':('title',)}
 
 admin.site.register(Category, CategoryAdmin)
+
+class EntryAdmin(admin.ModelAdmin):
+	model = Entry
+	prepopulated_fields = {'slug':('title',)}
+
+admin.site.register(Entry, EntryAdmin)
--- cms/coltrane/models.py
+++ cms/coltrane/models.py
@@ -1,7 +1,10 @@
 from django.db import models
+from django.contrib.auth.models import User
+import datetime
 
 class Category(models.Model):
-	title = models.CharField(max_length=250)
+	title = models.CharField(max_length=250,help_text='Maximum 250 chars')
+	#Prepopulate_from moved to admin
 	slug = models.SlugField(unique=True)
 	description = models.TextField()
 
@@ -14,3 +17,31 @@ class Category(models.Model):
 
 	def __unicode__(self):
 		return self.title
+
+	def get_absolute_url(self):
+		return "/categories/%s/" % self.slug
+
+class Entry(models.Model):
+	LIVE_STATUS = 1
+	DRAFT_STATUS = 2
+	HIDDEN_STATUS = 3
+	STATUS_CHOICES = (
+                (LIVE_STATUS, 'Live'),
+                (DRAFT_STATUS, 'Draft'),
+		(HIDDEN_STATUS, 'Hidden'),
+        )	
+
+        class Meta:
+                verbose_name_plural="Entries"
+
+	title = models.CharField(max_length=250)
+	excerpt = models.TextField(blank=True)
+	body = models.TextField()
+	pub_date = models.DateTimeField()
+	slug = models.SlugField(default=datetime.datetime.now,
+				unique_for_date='pub_date')
+	author = models.ForeignKey(User)
+	enable_comments = models.BooleanField(default=True)
+	featured = models.BooleanField(default=False)
+	status = models.IntegerField(choices=STATUS_CHOICES, default=LIVE_STATUS)
+	categories = models.ManyToManyField(Category)
