83966af2f18c33e3e23426d968d29b2261c7f118
Drew Buschhorn <drewbuschhorn@gmail.com>
2012-08-04 23:43:05 -0400

    Adding models.py

---
+++ cms/coltrane/models.py
@@ -0,0 +1,16 @@
+from django.db import models
+
+class Category(models.Model):
+	title = models.CharField(max_length=250)
+	slug = models.SlugField(unique=True)
+	description = models.TextField()
+
+	#Kept but old style
+	#Class Admin:
+	#	pass
+
+	class Meta:
+		verbose_name_plural="Categories"	
+
+	def __unicode__(self):
+		return self.title
